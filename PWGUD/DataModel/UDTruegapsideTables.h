4775bfc0ae6ba17e3415346d263fef80af3be776
Luzhiyongg <71517277+Luzhiyongg@users.noreply.github.com>
2026-05-06 11:01:14 +0200

    description fix

--- PWGUD/DataModel/UDTruegapsideTables.h
+++ PWGUD/DataModel/UDTruegapsideTables.h
@@ -9,7 +9,7 @@
 // granted to it by virtue of its status as an Intergovernmental Organization
 // or submit itself to any jurisdiction.
 
-/// \file UpcTruegapsideTables.h
+/// \file UDTruegapsideTables.h
 /// \author Zhiyong Lu (zhiyong.lu@cern.ch)
 /// \since  Apr/7/2026
 /// \brief header defines truegapside outputs
